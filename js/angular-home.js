57f2483a447ad6f258cd29a796d1cd554b9ed826
Guillaume <geeyom12@gmail.com>
2021-09-21 18:06:24 +0200

    Update angular-home.js

--- js/angular-home.js
+++ js/angular-home.js
@@ -5,25 +5,25 @@ angular.module('diabolos',
 		spectaclesList.spectacles = [
 			{
 				name:"La Nuit du Théâtre fait son Kabaraoké", 
-				date: new Date(Date.UTC(2021,09,26,0,0)), 
+				date: new Date(Date.UTC(2021,08,26,0,0)), 
 				venue: "La Fabrique à impros (Nantes)", 
 				image:"https://mcusercontent.com/5e087c6d40f3081c7f733c309/images/063539f0-7df3-536c-525e-f1528415e356.jpeg",
 				link:"https://theatresnantais.wixsite.com/home/billetterie"
 			},{
 				name:"Putaclic",
-				date: new Date(Date.UTC(2021,10,9,18,30)),
+				date: new Date(Date.UTC(2021,9,9,18,30)),
 				venue: "La Fabrique à impros (Nantes)",
 				image:"https://mcusercontent.com/5e087c6d40f3081c7f733c309/images/ba1b9896-a7e2-a842-55da-09663aebf32a.jpg",
 				link: "https://lafabriqueaimpros.com/programmation/#events"
 			},{
 				name:"Catch d'impro à l'anglaise",
-				date:new Date(Date.UTC(2021,10,16,20,30)),
+				date:new Date(Date.UTC(2021,9,16,20,30)),
 				venue: "Le Dix (Nantes)",
 				image:"https://mcusercontent.com/5e087c6d40f3081c7f733c309/images/37533d88-7975-cecd-5bb1-db7a6767d4c0.jpg",
 				link: "https://www.facebook.com/events/121428595210594/"
 			},{
 				name:"Incroyables voyages",
-				date:new Date(Date.UTC(2021,10,20,16,30)),
+				date:new Date(Date.UTC(2021,9,20,16,30)),
 				venue: "Médiathèque René Goscinny (Sainte Luce-sur-Loire)",
 				image:"https://gallery.mailchimp.com/5e087c6d40f3081c7f733c309/images/7d6d852b-5ce7-4ddb-ab20-066a6413efb8.jpg",
 				link:"http://mediatheque.sainte-luce-loire.com/actualite/15-ans"
