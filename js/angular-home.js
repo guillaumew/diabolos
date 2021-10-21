d355ae9c523849a32adf7e4fcbf81f7b46444dba
Guillaume <geeyom12@gmail.com>
2021-10-21 08:54:30 +0200

    Scenes de ménage

--- js/angular-home.js
+++ js/angular-home.js
@@ -4,30 +4,6 @@ angular.module('diabolos',
 		var spectaclesList = this;
 		spectaclesList.spectacles = [
 			{
-				name:"La Nuit du Théâtre fait son Kabaraoké", 
-				date: new Date(Date.UTC(2021,08,26,0,0)), 
-				venue: "La Fabrique à impros (Nantes)", 
-				image:"https://mcusercontent.com/5e087c6d40f3081c7f733c309/images/063539f0-7df3-536c-525e-f1528415e356.jpeg",
-				link:"https://theatresnantais.wixsite.com/home/billetterie"
-			},{
-				name:"Putaclic",
-				date: new Date(Date.UTC(2021,9,9,18,30)),
-				venue: "La Fabrique à impros (Nantes)",
-				image:"https://mcusercontent.com/5e087c6d40f3081c7f733c309/images/ba1b9896-a7e2-a842-55da-09663aebf32a.jpg",
-				link: "https://lafabriqueaimpros.com/programmation/#events"
-			},{
-				name:"Catch d'impro à l'anglaise",
-				date:new Date(Date.UTC(2021,9,16,20,30)),
-				venue: "Le Dix (Nantes)",
-				image:"https://mcusercontent.com/5e087c6d40f3081c7f733c309/images/37533d88-7975-cecd-5bb1-db7a6767d4c0.jpg",
-				link: "https://www.facebook.com/events/121428595210594/"
-			},{
-				name:"Incroyables voyages",
-				date:new Date(Date.UTC(2021,9,20,16,30)),
-				venue: "Médiathèque René Goscinny (Sainte Luce-sur-Loire)",
-				image:"https://gallery.mailchimp.com/5e087c6d40f3081c7f733c309/images/7d6d852b-5ce7-4ddb-ab20-066a6413efb8.jpg",
-				link:"http://mediatheque.sainte-luce-loire.com/actualite/programme-de-lanniversaire-15-ans-de-mediatheque-devoile"
-			},{
 				name:"Politiquement incorrect",
 				date:new Date(Date.UTC(2021,10,06,18,30)),
 				venue: "La Fabrique à impros",
@@ -45,6 +21,12 @@ angular.module('diabolos',
 				venue: "La Fabrique à impros",
 				image:"https://lafabriqueaimpros.com/wp-content/uploads/2021/10/AFFICHE-IDVELH_2-scaled.jpg",
 				link:"https://lafabriqueaimpros.com/%C3%A9v%C3%A8nement/limpro-dont-vous-etes-le-heros/"
+			},{
+				name:"Scènes de ménage",
+				date:new Date(Date.UTC(2021,10,03,20,30)),
+				venue: " Belle de Jour",
+				image:"https://mcusercontent.com/5e087c6d40f3081c7f733c309/images/234cc8a4-3de9-423a-e498-831a60cd0047.png",
+				link:"https://www.facebook.com/events/276613521019350/"
 			}
 		];
 	})
