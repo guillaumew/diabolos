c3c780801ea57d207d25e5b18bdd8bdf748a8b04
Guillaume <geeyom12@gmail.com>
2022-02-03 09:31:21 +0100

    Add Autour du livre shows

--- js/angular-home.js
+++ js/angular-home.js
@@ -9,6 +9,18 @@ angular.module('diabolos',
 				venue: "Montaigu (85)",
 				image:"https://mcusercontent.com/5e087c6d40f3081c7f733c309/images/df99f39d-0c21-7332-c218-f4674b447cc0.jpeg",
 				link:"https://www.helloasso.com/associations/association-heyoka/evenements/les-hilairantes"
+			},{
+				name:"Autour du livre",
+				date:new Date(Date.UTC(2022,01,26,15,00)),
+				venue: "Clisson (44)",
+				image:"https://gallery.mailchimp.com/5e087c6d40f3081c7f733c309/images/7d6d852b-5ce7-4ddb-ab20-066a6413efb8.jpg",
+				link:"https://www.mediatheque-clisson.net/cms/articleview/id_profil/29/id/320"
+			},{
+				name:"Autour du livre",
+				date:new Date(Date.UTC(2022,03,6,16,00)),
+				venue: "Nantes (44)",
+				image:"https://gallery.mailchimp.com/5e087c6d40f3081c7f733c309/images/7d6d852b-5ce7-4ddb-ab20-066a6413efb8.jpg",
+				link:"https://www.facebook.com/events/241036584868683/"
 			}
 		];
 	})
