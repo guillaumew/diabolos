8af307587952333b883214a48ea0a93c5d85f1bf
Guillaume <geeyom12@gmail.com>
2022-02-03 09:34:27 +0100

    Oups wrong date

--- js/angular-home.js
+++ js/angular-home.js
@@ -17,7 +17,7 @@ angular.module('diabolos',
 				link:"https://www.mediatheque-clisson.net/cms/articleview/id_profil/29/id/320"
 			},{
 				name:"Autour du livre",
-				date:new Date(Date.UTC(2022,03,6,16,00)),
+				date:new Date(Date.UTC(2022,02,6,16,00)),
 				venue: "Nantes (44)",
 				image:"https://gallery.mailchimp.com/5e087c6d40f3081c7f733c309/images/7d6d852b-5ce7-4ddb-ab20-066a6413efb8.jpg",
 				link:"https://www.facebook.com/events/241036584868683/"
