0a6a38aed1f08cd7b9a4795ba3c413a03f05b214
Guillaume <geeyom12@gmail.com>
2022-06-24 09:38:03 +0200

    Update angular-home.js

--- js/angular-home.js
+++ js/angular-home.js
@@ -7,7 +7,7 @@ angular.module('diabolos',
 				name:"Politiquement incorrect",
 				date:new Date(Date.UTC(2022,05,11,19,00)),
 				venue: "Salle du Jamet - 25 rue du Jamet - Nantes (44)",
-				Price: "tarif : 7 €"
+				Price: "tarif : 7 €",
 				image:"https://lafabriqueaimpros.com/wp-content/uploads/2022/04/politiquement-incorrect.jpg",
 				link:"https://lekiosquenantais.fr/l-agenda/politiquement-incorrect-la-replique"
 				
@@ -15,7 +15,7 @@ angular.module('diabolos',
 				name:"Festival l'envolée - catch impro",
 				date:new Date(Date.UTC(2022,05,24,21,15)),
 				venue: "La tour à Plomb - Couëron (44)",
-				Price: "Tarif : 8€"
+				Price: "Tarif : 8€",
 				image:"https://mcusercontent.com/5e087c6d40f3081c7f733c309/images/3c0e6509-1456-a12d-4a38-5090e0a7b3ce.png",
 				link:"https://www.lesluciolesassociation.fr/programmation-festival-lenvolee/"
 			}
