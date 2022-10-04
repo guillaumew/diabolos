2c06a1742c1542859d61be7a3cf9e9b7df70d29a
diabolosnantes <101512130+diabolosnantes@users.noreply.github.com>
2022-10-05 01:08:02 +0200

    Dates octobre 2022

--- js/angular-home.js
+++ js/angular-home.js
@@ -5,19 +5,14 @@ angular.module('diabolos',
 		spectaclesList.spectacles = [
 			{
 				name:"Politiquement incorrect",
-				date:new Date(Date.UTC(2022,05,11,19,00)),
-				venue: "Salle du Jamet - 25 rue du Jamet - Nantes (44)",
-				Price: "tarif : 7 €",
+				date:new Date(Date.UTC(2022,22,09,18,30)),
+				venue: "La Fabrique à impros - 14 rue de l'arche sèche - Nantes",
+				Price: "tarif : 8,99 €",
 				image:"https://lafabriqueaimpros.com/wp-content/uploads/2022/04/politiquement-incorrect.jpg",
-				link:"https://lekiosquenantais.fr/l-agenda/politiquement-incorrect-la-replique"
+				link:"https://lekiosquenantais.fr/l-agenda/politiquement-incorrect"
 				
 			},{
-				name:"Festival l'envolée - catch impro",
-				date:new Date(Date.UTC(2022,05,24,21,15)),
-				venue: "La tour à Plomb - Couëron (44)",
-				Price: "Tarif : 8€",
-				image:"https://mcusercontent.com/5e087c6d40f3081c7f733c309/images/3c0e6509-1456-a12d-4a38-5090e0a7b3ce.png",
-				link:"https://www.lesluciolesassociation.fr/programmation-festival-lenvolee/"
+				
 			}
 			
 		];
