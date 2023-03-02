64d8e1ed155d3c824170bb323c8e15b7eac3e4e3
diabolosnantes <101512130+diabolosnantes@users.noreply.github.com>
2023-03-02 01:43:42 +0100

    Mise à jour des dates pour mars 2023

--- js/angular-home.js
+++ js/angular-home.js
@@ -5,38 +5,21 @@ angular.module('diabolos',
 		spectaclesList.spectacles = [
 			
 			{
+				name:"Thérapie de couples",
+				date:new Date(Date.UTC(2023,02,11,18,30)),
+				venue: "La Fabrique à impros - 14 rue de l'arche sèche - Nantes",
+				Price: "tarif : 8€",
+				image:"https://mcusercontent.com/5e087c6d40f3081c7f733c309/images/7b3ce511-bb9d-87e2-6fc7-286ad6c3be55.jpg",
+				link:"https://lafabriqueaimpros.com/produit/therapie-de-couples/"
+			},
+			{
 				name:"Les jeudis c'est Derby",
-				date:new Date(Date.UTC(2023,00,05,21,00)),
+				date:new Date(Date.UTC(2023,02,23,21,00)),
 				venue: "La Fabrique à impros - 14 rue de l'arche sèche - Nantes",
 				Price: "tarif : 8€",
 				image:"https://mcusercontent.com/5e087c6d40f3081c7f733c309/images/1a087a03-8d2d-6208-4f36-59721e5af934.jpeg",
-				link:"https://lafabriqueaimpros.com/produit/les-jeudis-cest-derby-9/"
+				link:"https://lafabriqueaimpros.com/produit/les-jeudis-cest-derby-16/"
 				
-			},
-			{
-				name:"L'impro dont vous êtes le héros",
-				date:new Date(Date.UTC(2023,00,14,18,30)),
-				venue: "La Fabrique à impros - 14 rue de l'arche sèche - Nantes",
-				Price: "tarif : 8€",
-				image:"https://mcusercontent.com/5e087c6d40f3081c7f733c309/images/4ef4ad6b-76ca-ee86-a1cf-4043eb069bae.jpg",
-				link:"https://lafabriqueaimpros.com/produit/limpro-dont-vous-etes-le-heros/"
-			},
-			 {
-				name:"La vie secrète des livres",
-				date:new Date(Date.UTC(2023,00,20,19,00)),
-				venue: "Médiathèque de Saint Aignan de Grand Lieu - 39 Rue des Frères Rousseau",
-				Price: "Entrée libre",
-				image:"https://mcusercontent.com/5e087c6d40f3081c7f733c309/images/ce6e8167-a7f5-cdca-61a1-2e777c947c03.jpg",
-				link:"https://www.facebook.com/events/934219190900867"
-				
-			},
-			{
-				name:"Match avec l'Alligator de Bordeaux",
-				date:new Date(Date.UTC(2023,00,21,20,30)),
-				venue: "Union Saint Bruno - 49 Rue Brizard, Bordeaux",
-				Price: "tarif : 8€",
-				image:"https://mcusercontent.com/5e087c6d40f3081c7f733c309/images/10464ad1-b749-abe2-3488-17af57999985.jpg",
-				link:"https://www.helloasso.com/associations/union-saint-bruno/evenements/match-d-impro-diabolos-nantes-vs-l-alligator"
 			},
 		];
 	})
